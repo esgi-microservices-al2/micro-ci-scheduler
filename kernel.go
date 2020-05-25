38a0034e8a5c33bdae6f865ae0a25cf4e509b8a8
Ajtene Kurtaliqi <ajtene.kurtaliqi@gmail.com>
2020-05-25 19:49:34 +0200

    docker-compose and config.json rectify

--- kernel.go
+++ kernel.go
@@ -5,9 +5,10 @@ import (
 	"micro-ci-scheduler/http/route"
 
 	"github.com/System-Glitch/goyave/v2"
+	"fmt"
 	// Import the approriate GORM dialect for the database you're using.
 	// _ "github.com/jinzhu/gorm/dialects/mysql"
-	// _ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	// _ "github.com/jinzhu/gorm/dialects/sqlite"
 	// _ "github.com/jinzhu/gorm/dialects/mssql"
 )
@@ -18,5 +19,10 @@ func main() {
 	// if you are running multiple services, such as a
 	// websocket server, you'll need to run Goyave in a routine.
 	// See: https://system-glitch.github.io/goyave/guide/advanced/multi-services.html
+
+	goyave.RegisterStartupHook(func() {
+		fmt.Println("Started")
+	})
+
 	goyave.Start(route.Register)
 }
